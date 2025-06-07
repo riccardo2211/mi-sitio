da0c26b4d932577c7fa77406e0fbc7c41bc2f22f
riccardo2211 <camolino76@gmail.com>
2025-06-07 17:16:30 +0200

    Create stackbit.config.ts

---
+++ stackbit.config.ts
@@ -0,0 +1,25 @@
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
+
+export default defineStackbitConfig({
+  stackbitVersion: "~0.6.0",
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content/pages"],
+      models: [
+        {
+          name: "page",
+          type: "page",
+          filePath: "content/pages/{slug}.md",
+          urlPath: "/{slug}",
+          fields: [
+            { name: "title", type: "string", required: true },
+            { name: "slug", type: "string", required: true },
+            { name: "body", type: "markdown" }
+          ]
+        }
+      ]
+    })
+  ]
+});
