6b37b7db2e2d47b2e02d9210701fc081682b18b5
riccardo2211 <camolino76@gmail.com>
2025-06-18 19:09:10 +0200

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,10 +1,12 @@
- main
 // stackbit.config.ts
 import { defineStackbitConfig } from "@stackbit/types";
 import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  // ...
+  stackbitVersion: "~0.6.0",
+  nodeVersion: "18",
+  ssgName: "custom",
+  postInstallCommand: "npm i --no-save @stackbit/types",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
@@ -12,23 +14,16 @@ export default defineStackbitConfig({
       models: [
         {
           name: "Page",
-          // Define the model as a page model
           type: "page",
           urlPath: "/{slug}",
           filePath: "content/pages/{slug}.json",
-          fields: [{ name: "title", type: "string", required: true }]
+          fields: [
+            { name: "title", type: "string", required: true }
+          ]
         }
-      ],
+      ]
     })
   ]
 });
 
-import { defineStackbitConfig } from '@stackbit/types';
 
-export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "custom",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-}) preview
