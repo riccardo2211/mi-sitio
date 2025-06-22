899d77a5a6e991108b5e17e126da1612626bccf0
riccardo2211 <camolino76@gmail.com>
2025-06-22 15:06:36 +0100

    Add files via upload

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,49 +1,25 @@
-import { defineStackbitConfig } from '@stackbit/types';
-import { GitContentSource } from '@stackbit/cms-git';
+
+import { defineStackbitConfig } from "@stackbit/types";
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
-  stackbitVersion: '~0.6.0',
-preview
-  nodeVersion: '18',
-  ssgName: 'custom',
+  stackbitVersion: "~0.6.0",
   contentSources: [
     new GitContentSource({
       rootPath: __dirname,
-      contentDirs: ['content'],
-
-  contentSources: [
-    new GitContentSource({
-      rootPath: __dirname,
-      contentDirs: ['content/pages'],
- main
+      contentDirs: ["content/pages"],
       models: [
         {
-          name: 'page',
-          type: 'page' preview
-          labelField: 'title'
- main
-          filePath: 'content/pages/{slug}.md',
-          urlPath: '/{slug}',
+          name: "page",
+          type: "page",
+          filePath: "content/pages/{slug}.md",
+          urlPath: "/{slug}",
           fields: [
-            { name: 'title', type: 'string', required: true },
- preview
-            { name: 'slug', type: 'string', required: true }
+            { name: "title", type: "string", required: true },
+            { name: "body", type: "markdown", label: "Contenido" }
           ]
         }
       ]
     })
-  ],
-  postInstallCommand: 'npm i --no-save @stackbit/types'
-
-            { name: 'body', type: 'markdown' },
-          ],
-        },
-      ],
-    }),
-  ],
-main
+  ]
 });
-
- 
-
-
